a478ebbca7f391d1089dcb4bab41bcaa1cbfb79a
Marcos Krol Pacheco <marcos.krolp@gmail.com>
2024-09-10 21:58:35 -0300

    Neste arquivo foi desenvolvido os exercício com o tipo Inteiro e praticado as operações de soma, divisão, modulo, multiplicação e exponenciação proposto pela jornada de dados.

---
+++ aula02/exercicios_inteiros.py
@@ -0,0 +1,45 @@
+# Exercícios Aula 02
+# Inteiro (int)
+# 1) Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+
+# soma = num1 + num2
+
+# print(f"A soma dos números solicitados é {soma}")
+
+
+# 2) Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+# num1 = int(input("Digite um número: "))
+
+# resto = num1 % 5
+
+# print(f"O resto da divisão de {num1} por 5 é: {resto}")
+
+# 3) Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+
+# multiplicacao = num1 * num2
+
+# print(f"O resultado da multiplicação entre {num1} e {num2} é de {multiplicacao}")
+
+# 4) Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+# num1 = int(input("Digite um número: "))
+# num2 = int(input("Digite outro número: "))
+
+# divisao = int(num1/num2)
+
+# print(f"A divisão entre {num1} e {num2} é de {divisao}")
+
+# 5) Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+num1 = int(input("Digite um número: "))
+
+quadrado = num1**2
+
+print(f"O quadro de {num1} é {quadrado}")
