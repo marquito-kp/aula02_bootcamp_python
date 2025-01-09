c8e26e31d6f78c58ebfb242739f7cda9bce7dece
Marcos Krol Pacheco <marcos.krolp@gmail.com>
2025-01-08 21:24:19 -0300

    Criação de uma calculadora simples com try-except, while e utilizando o argumento raise para forçar um erro ao não selecionar um operador valido.

---
+++ calculadora_simples.py
@@ -0,0 +1,35 @@
+# Exercício 23: Calculadora Simples
+# Desenvolva uma calculadora simples que aceite duas entradas numéricas e um operador (+, -, *, /) do usuário. 
+# Use try-except para lidar com divisões por zero e entradas não numéricas. Utilize if-elif-else para realizar a operação 
+# matemática baseada no operador fornecido. Imprima o resultado ou uma mensagem de erro apropriada.
+
+print("# CALCULDORA SIMPLES #")
+
+while True:
+    num1 = float(input("Digite um número: "))
+    num2 = float(input("Digite outro número: "))
+    try:
+        operador = input("Selecione o operador da conta:\n (+) - soma\n (-) - Subtração\n (*) - Multiplicação\n (/) - Divisão\nSua escolha: ")
+        if operador not in ['+','-','*','/']:
+            raise ValueError("Operador inválido.")
+    except ValueError as e:
+        print(f"{e} Tente novamente!")
+    if operador == "+":
+        soma = num1 + num2
+        print(f"Resultado: {soma}")
+        break
+    elif operador == "-":
+        subtração = num1 - num2
+        print(f"Resultado: {subtração}")
+        break
+    elif operador == "*":
+        multiplicacao = num1 * num2
+        print(f"Resultado: {multiplicacao}")
+        break
+    elif operador == "/":
+        try:
+            divisao = num1 / num2
+            print(f"Resultado: {divisao}")
+            break
+        except ZeroDivisionError:
+            print("O número não pode ser dividido por 0 (zero).")
